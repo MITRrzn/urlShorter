6a55ed1dc42a3e36d75c37b95fb43ddf9d6f6cfd
mitroshin.R <mitr.rzn@gmail.com>
2026-09-06 23:15:31 +0300

    fix stat service

--- internal/service/stats/statsHandler.go
+++ internal/service/stats/statsHandler.go
@@ -2,14 +2,17 @@ package stats
 
 import (
 	"database/sql"
+	"encoding/json"
+	"log"
 	"net/http"
 	"urlShorter/internal/helper"
 	"urlShorter/internal/repository"
+	"urlShorter/internal/structs"
 )
 
 func StatsHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		code := r.FormValue("code")
+		code := r.PathValue("code")
 		validationErr := helper.ValidateCode(code)
 		if validationErr != nil {
 			helper.WriteErrorResponse(w, validationErr.Error(), http.StatusBadRequest)
@@ -18,11 +21,20 @@ func StatsHandler(db *sql.DB) http.HandlerFunc {
 
 		stats, err := repository.GetStatByCode(r.Context(), db, code)
 		if err != nil {
-			helper.WriteErrorResponse(w, err.Error(), http.StatusInternalServerError)
+			helper.WriteErrorResponse(w, err.Error(), http.StatusNotFound)
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
+		encodeErr := json.NewEncoder(w).Encode(structs.Stats{
+			ShortURL:     stats.ShortURL,
+			ClicksAmount: stats.ClicksAmount,
+			Daily:        stats.Daily,
+		})
+		if encodeErr != nil {
+			log.Println(encodeErr)
+			return
+		}
 		return
 	}
 }
