9fa8e50eb0980a9f24d3adcda9f9c70574ce693b
mitroshin.R <mitr.rzn@gmail.com>
2026-09-06 23:20:07 +0300

    fix stat service

--- internal/service/stats/statsHandler.go
+++ internal/service/stats/statsHandler.go
@@ -3,11 +3,11 @@ package stats
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"urlShorter/internal/helper"
 	"urlShorter/internal/repository"
-	"urlShorter/internal/structs"
 )
 
 func StatsHandler(db *sql.DB) http.HandlerFunc {
@@ -20,17 +20,18 @@ func StatsHandler(db *sql.DB) http.HandlerFunc {
 		}
 
 		stats, err := repository.GetStatByCode(r.Context(), db, code)
-		if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
 			helper.WriteErrorResponse(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		if err != nil {
+			helper.WriteErrorResponse(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		encodeErr := json.NewEncoder(w).Encode(structs.Stats{
-			ShortURL:     stats.ShortURL,
-			ClicksAmount: stats.ClicksAmount,
-			Daily:        stats.Daily,
-		})
+		encodeErr := json.NewEncoder(w).Encode(stats)
 		if encodeErr != nil {
 			log.Println(encodeErr)
 			return
